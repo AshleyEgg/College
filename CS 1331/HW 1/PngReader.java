9dc4ce095cebc0ec919d39072045edc0629b9709
Ashley <39559276+AshleyEgg@users.noreply.github.com>
2018-05-30 15:35:55 -0400

    Add files via upload

---
+++ CS 1331/HW 1/PngReader.java
@@ -0,0 +1,14 @@
+public class PngReader{
+    public static void main (String[] args){
+
+    }
+
+    public static String tagValue(String tagName, String game){
+
+    }
+
+    public static String finalPosition(String game){
+
+    }
+
+}
