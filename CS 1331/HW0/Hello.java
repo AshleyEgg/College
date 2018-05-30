9f6b27a55e3d7b905801d43de1a29c3277cfb25a
Ashley <39559276+AshleyEgg@users.noreply.github.com>
2018-05-30 15:26:51 -0400

    Delete Hello.java

--- CS 1331/HW0/Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-        double result = (double) 2/3;
-        System.out.println(result);// Click pl tab to get System.out.println() in Sublime;
-    }
-}
