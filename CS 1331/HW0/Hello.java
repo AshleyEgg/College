bd7670441af4594452bb755e38d4977f451f3a93
Ashley <39559276+AshleyEgg@users.noreply.github.com>
2018-05-30 12:59:15 -0400

    Add files via upload

---
+++ CS 1331/HW0/Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        double result = (double) 2/3;
+        System.out.println(result);// Click pl tab to get System.out.println();
+    }
+}
