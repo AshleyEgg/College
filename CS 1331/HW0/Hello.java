63cc1b0672d95b5592e0ead7e743a0c7c714f0d9
Ashley <39559276+AshleyEgg@users.noreply.github.com>
2018-05-30 15:15:53 -0400

    Update Hello.java

--- CS 1331/HW0/Hello.java
+++ CS 1331/HW0/Hello.java
@@ -2,6 +2,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello World!");
         double result = (double) 2/3;
-        System.out.println(result);// Click pl tab to get System.out.println();
+        System.out.println(result);// Click pl tab to get System.out.println() in Sublime;
     }
 }
